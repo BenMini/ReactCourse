b5f8004925be31385619d7aa01da128c0d9b9373
Ben Minifie <benrs6mini@gmail.com>
2020-07-03 18:51:05 -0400

    feat: Header, Order, Inventory Components

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -5,8 +5,9 @@ class StorePicker extends React.Component {
     return (
       <>
         <form action="" className="store-selector">
+          <h1>Please Enter A Store</h1>
           <input type="text" required placeholder="Store Name" />
-          <button type="submit">Visit Store</button>
+          <button type="submit">Visit Store →</button>
         </form>
       </>
     );
